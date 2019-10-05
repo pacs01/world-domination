8ae924208d13069315d1cf4cdfbbcbfe8c830db0
Patrick Scherler <pacs01dev@gmail.com>
2019-10-05 13:44:39 +0200

    fix jackson deserialization

--- src/main/java/io/scherler/games/risk/models/request/Territory.java
+++ src/main/java/io/scherler/games/risk/models/request/Territory.java
@@ -3,9 +3,11 @@ package io.scherler.games.risk.models.request;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Territory {
 
     @NotBlank(message = "Territory name is mandatory")
--- src/main/java/io/scherler/games/risk/models/request/UserAccount.java
+++ src/main/java/io/scherler/games/risk/models/request/UserAccount.java
@@ -3,9 +3,11 @@ package io.scherler.games.risk.models.request;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserAccount {
 
     @NotBlank(message = "Username is mandatory")
