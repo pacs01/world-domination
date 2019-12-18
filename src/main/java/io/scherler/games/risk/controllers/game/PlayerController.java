d7e2868ce8d1b474ebed81de67a55e76c1ba4947
Patrick Scherler <pacs01dev@gmail.com>
2019-12-18 01:08:38 +0100

    implement player creation endpoint

--- src/main/java/io/scherler/games/risk/controllers/game/PlayerController.java
+++ src/main/java/io/scherler/games/risk/controllers/game/PlayerController.java
@@ -5,8 +5,13 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import io.scherler.games.risk.controllers.defaults.NestedResourceAssembler;
 import io.scherler.games.risk.controllers.defaults.NestedResourceController;
+import io.scherler.games.risk.models.request.identity.UserRequest;
 import io.scherler.games.risk.models.response.game.PlayerInfo;
+import io.scherler.games.risk.services.game.GameService;
 import io.scherler.games.risk.services.game.PlayerService;
+import io.scherler.games.risk.services.identity.UserAccountService;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
 import lombok.val;
 import org.springframework.hateoas.Resource;
@@ -15,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +30,15 @@ public class PlayerController implements NestedResourceController<PlayerInfo> {
 
     private final NestedResourceAssembler<PlayerInfo> resourceAssembler;
     private final PlayerService playerService;
+    private final GameService gameService;
+    private final UserAccountService userAccountService;
 
-    public PlayerController(PlayerService playerService) {
+    public PlayerController(PlayerService playerService,
+        GameService gameService,
+        UserAccountService userAccountService) {
         this.playerService = playerService;
+        this.gameService = gameService;
+        this.userAccountService = userAccountService;
         this.resourceAssembler = new NestedResourceAssembler<>(this);
     }
 
@@ -54,5 +66,14 @@ public class PlayerController implements NestedResourceController<PlayerInfo> {
         return ResponseEntity.noContent().build();
     }
 
-    // todo implement create() method
+    @PostMapping()
+    public ResponseEntity<?> createNew(@PathVariable("gameId") Long gameId)
+        throws URISyntaxException {
+        val userAccount = userAccountService.get(1); //todo: load user from http authorization
+        val game = gameService.get(gameId);
+        val player = playerService.create(new UserRequest<>(game, userAccount));
+        Resource<PlayerInfo> resource = resourceAssembler.toResource(PlayerInfo.from(player));
+
+        return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
+    }
 }
